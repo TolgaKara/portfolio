e6592a0bb3536594e025a7e87f689223ceebef4e
TolgaKara <tolgakara2.0@gmail.com>
2020-12-29 19:29:24 +0100

    Added a Landingpage

--- src/components/Landingpage.jsx
+++ src/components/Landingpage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 export default function Landingpage() {
-	return <div>Hello</div>;
+	return <div>Landingpage</div>;
 }
