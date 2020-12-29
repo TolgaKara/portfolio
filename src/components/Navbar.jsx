4027932016d1da3492bc35200b97415aa1414797
TolgaKara <tolgakara2.0@gmail.com>
2020-12-29 19:29:49 +0100

    Updated Navbar still styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,22 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
 export const Navbar = () => {
-	return <div></div>;
+	return (
+		<header className='bg-red-600'>
+			<div className='container mx-auto flex justify-between'>
+				<nav>
+					<NavLink to='/' exact>
+						Tolga Kara
+					</NavLink>
+					<NavLink to='/about'>ABOUT</NavLink>
+					<NavLink to='/workexperience'>WORK EXPERIENCE</NavLink>
+					<NavLink to='/project'>PROJECT</NavLink>
+					<NavLink to='/education'>EDUCATION</NavLink>
+					<NavLink to='/faq'>FAQ</NavLink>
+					<NavLink to='/blog'>BLOG</NavLink>
+					<NavLink to='/contactme'>CONTACT ME</NavLink>
+				</nav>
+			</div>
+		</header>
+	);
 };
