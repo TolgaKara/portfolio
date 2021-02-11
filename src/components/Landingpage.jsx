505e3af82dabd4ebe135efcb6e6534a0f4cd2a86
TolgaKara <tolgakara2.0@gmail.com>
2021-02-11 19:49:53 +0100

    add bg image to home

--- src/components/Landingpage.jsx
+++ src/components/Landingpage.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import bgImage from "../images/bg_minimalism_origami_japan.jpg";
 
 export default function Landingpage() {
 	return (
 		<main>
-			<img src='' alt='' />
-			<section>
-				<h1>Welcome, I am Tolga Kara</h1>
+			<img src={bgImage} alt='' className='absolute object-cover w-full h-full' />
+			<section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
+				<h1 className='text-6xl text-white bg-red-800 h-full px-4 shadow-lg font-bold cursive leading-none lg:leading-snug home-name'>
+					Welcome, I am Tolga Kara
+				</h1>
 			</section>
 		</main>
 	);
