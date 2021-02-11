0a9686f6254a8897c1acb4fe0e4b4cd3476daa09
TolgaKara <tolgakara2.0@gmail.com>
2021-02-11 17:48:42 +0100

    Added bg image to the image folder

--- src/components/Landingpage.jsx
+++ src/components/Landingpage.jsx
@@ -2,5 +2,12 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 export default function Landingpage() {
-	return <div>Landingpage</div>;
+	return (
+		<main>
+			<img src='' alt='' />
+			<section>
+				<h1>Welcome, I am Tolga Kara</h1>
+			</section>
+		</main>
+	);
 }
