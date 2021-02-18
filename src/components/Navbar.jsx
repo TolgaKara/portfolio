38e601089a168d37b954e47917fe698c264ebdec
TolgaKara <tolgakara2.0@gmail.com>
2021-02-18 13:50:07 +0100

    Updated the navbar with logo and overall colors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,72 +1,72 @@
 import { SocialIcon } from "react-social-icons";
 import React from "react";
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+import { Logo } from "./Logo";
 
 export default function Navbar() {
 	return (
-		<header className='bg-red-600'>
-			<div className='container mx-auto flex justify-between'>
+		<header className='bg-white shadow'>
+			<div className='container mx-auto flex justify-between text-green-800'>
 				<nav>
 					<NavLink
-						className='inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest'
+						className='inflex-flex items-center py-6 px-3 mr-4 font-bold cursive tracking-widest'
 						to='/'
-						activeClassName='text-white'
 						exact
 					>
-						Tolga Kara
+						<Logo />
 					</NavLink>
 					<NavLink
 						to='/about'
-						className='inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-white font-bold'
-						activeClassName='text-red-100 bg-red-700'
+						className='inline-flex items-center py-3 px-3 my-6 rounded text-black font-semibold hover:text-yellow-600 transition duration-300 ease-in-out'
+						activeClassName='text-yellow-600 border-b-2 border-yellow-600 font-bold'
 					>
 						ABOUT
 					</NavLink>
 					<NavLink
 						to='/workexperience'
-						className='inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-white font-bold'
-						activeClassName='text-red-100 bg-red-700'
+						className='inline-flex items-center py-3 px-3 my-6 rounded text-black font-semibold hover:text-yellow-600 transition duration-300 ease-in-out'
+						activeClassName='text-yellow-600 border-b-2 border-yellow-600 font-bold'
 					>
 						WORK EXPERIENCE
 					</NavLink>
 					<NavLink
 						to='/project'
-						className='inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-white font-bold'
-						activeClassName='text-red-100 bg-red-700'
+						className='inline-flex items-center py-3 px-3 my-6 rounded text-black font-semibold hover:text-yellow-600 transition duration-300 ease-in-out'
+						activeClassName='text-yellow-600 border-b-2 border-yellow-600 font-bold'
 					>
 						PROJECT
 					</NavLink>
 					<NavLink
 						to='/education'
-						className='inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-white font-bold'
-						activeClassName='text-red-100 bg-red-700'
+						className='inline-flex items-center py-3 px-3 my-6 rounded text-black font-semibold hover:text-yellow-600 transition duration-300 ease-in-out'
+						activeClassName='text-yellow-600 border-b-2 border-yellow-600 font-bold'
 					>
 						EDUCATION
 					</NavLink>
 					<NavLink
 						to='/faq'
-						className='inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-white font-bold'
-						activeClassName='text-red-100 bg-red-700'
+						className='inline-flex items-center py-3 px-3 my-6 rounded text-black font-semibold hover:text-yellow-600 transition duration-300 ease-in-out'
+						activeClassName='text-yellow-600 border-b-2 border-yellow-600 font-bold'
 					>
 						FAQ
 					</NavLink>
 					<NavLink
 						to='/blog'
-						className='inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-white font-bold'
-						activeClassName='text-red-100 bg-red-700'
+						className='inline-flex items-center py-3 px-3 my-6 rounded text-black font-semibold hover:text-yellow-600 transition duration-300 ease-in-out'
+						activeClassName='text-yellow-600 border-b-2 border-yellow-600 font-bold'
 					>
 						BLOG
 					</NavLink>
 					<NavLink
 						to='/contactme'
-						className='inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-white font-bold'
-						activeClassName='text-red-100 bg-red-700'
+						className='inline-flex items-center py-3 px-3 my-6 rounded text-black font-semibold hover:text-yellow-600 transition duration-300 ease-in-out'
+						activeClassName='text-yellow-600 border-b-2 border-yellow-600 font-bold'
 					>
 						CONTACT ME
 					</NavLink>
 				</nav>
 				<div className='inline-flex py-3 px-3 my-6'>
-					<SocialIcon
+					{/* <SocialIcon
 						url='https://www.linkedin.com/in/tolgakara/'
 						className='mr-4'
 						target='_blank'
@@ -79,7 +79,7 @@ export default function Navbar() {
 						target='_blank'
 						fgColor='#fff'
 						style={{ height: 35, width: 35 }}
-					/>
+					/> */}
 				</div>
 			</div>
 		</header>
