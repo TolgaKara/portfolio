88ec7bb9bd7ac2e2c130bc2a755b60a29715621c
TolgaKara <tolgakara2.0@gmail.com>
2021-02-18 21:08:11 +0100

    Added Hire Btn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { SocialIcon } from "react-social-icons";
 import React from "react";
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 import { Logo } from "./Logo";
-import { FaLinkedin } from "react-icons/fa";
+import { FaGithubAlt, FaLinkedin } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
@@ -68,13 +68,19 @@ export default function Navbar() {
 					</NavLink>
 				</nav>
 				<div className='inline-flex py-3 px-3 my-6'>
-					<a href='https://www.linkedin.com/in/tolgakara/' target='_blank'>
-						<FaLinkedin className='text-yellow-600 text-2xl hover:text-yellow-700 cursor-pointer' />
+					<a href='https://github.com/TolgaKara' target='_blank'>
+						<FaGithubAlt className='text-yellow-600 text-2xl hover:text-yellow-700 cursor-pointer mr-4' />
 					</a>
 
 					<a href='https://www.linkedin.com/in/tolgakara/' target='_blank'>
-						<FaLinkedin className='text-yellow-600 text-2xl hover:text-yellow-700 cursor-pointer' />
+						<FaLinkedin className='text-yellow-600 text-2xl hover:text-yellow-700 cursor-pointer mr-4' />
 					</a>
+					<Link
+						className='bg-yellow-400 rounded px-5 text-yellow-600 font-semibold uppercase'
+						href=''
+					>
+						Hire
+					</Link>
 				</div>
 			</div>
 		</header>
