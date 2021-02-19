2346567719b0814d90ecefc2ceb61bcb40708dca
TolgaKara <tolgakara2.0@gmail.com>
2021-02-19 19:44:59 +0100

    Boilerplate code for about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import sanityClient from "../client.js";
 
 export default function About() {
-	return <div></div>;
+	const [author, setAuthor] = useState(null);
+
+	return (
+		<div className='container mx-auto'>
+			<div>
+				<h1>Tolga Kara</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi asperiores porro sint non,
+					aspernatur deleniti est! Accusantium nisi, cupiditate excepturi ex exercitationem nam,
+					quidem mollitia blanditiis doloremque, repudiandae fugiat maiores.
+				</p>
+			</div>
+			<div></div>
+		</div>
+	);
 }
