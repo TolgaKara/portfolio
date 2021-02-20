af7861cfdb088cc7a595f65f049587cde28f4c8f
TolgaKara <tolgakara2.0@gmail.com>
2021-02-20 10:13:09 +0100

    Added FAQ for Customers and Companies

---
+++ portfolio/schemas/faqCompany.js
@@ -0,0 +1,12 @@
+export default {
+	name: "faqCompany",
+	title: "FAQ for Companies",
+	type: "document",
+	fields: [
+		{
+			title: "What are your current responsibilities?",
+			name: "faq1",
+			type: "string",
+		},
+	],
+};
