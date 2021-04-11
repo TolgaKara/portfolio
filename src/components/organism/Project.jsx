40894e5135c797426f08f4f41e266de15b222882
Tolga Kara <tolgakara2.0@gmail.com>
2021-04-11 15:33:27 +0200

    Worked on Project

--- src/components/organism/Project.jsx
+++ src/components/organism/Project.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { ProjectCard } from "../molecules/ProjectCard";
 
 export default function Project() {
 	return (
 		<div className='container mx-auto flex justify-between items-center' id='about'>
-			Project
+			<ProjectCard />
 		</div>
 	);
 }
