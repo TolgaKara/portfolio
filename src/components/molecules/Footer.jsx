abe6419b5163aa8c4367cda56ab3f92e3e6bc972
Tolga Kara <tolgakara2.0@gmail.com>
2021-04-11 16:18:09 +0200

    Updated Footer and fixed aligned error

--- src/components/molecules/Footer.jsx
+++ src/components/molecules/Footer.jsx
@@ -3,10 +3,8 @@ import { Logo } from "../atoms/Logo";
 
 export const Footer = () => {
 	return (
-		<footer className=' text-sm flex items-center justify-center'>
-			<div className='absolute bottom-0 border-t border-gray-400 py-5 w-full text-center'>
-				<p className='mr-24'>© Tolga Kara</p>
-			</div>
+		<footer className='mt-10 bottom-0 border-t border-gray-400 py-5 w-full text-center text-sm flex items-center justify-center'>
+			<p className='mr-24'>© Tolga Kara</p>
 		</footer>
 	);
 };
