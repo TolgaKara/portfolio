d05608109c57a819780b1e948b56229f324e6580
Tolga Kara <tolgakara2.0@gmail.com>
2021-04-14 19:24:01 +0200

    updated Footer

--- src/components/molecules/Footer.jsx
+++ src/components/molecules/Footer.jsx
@@ -3,7 +3,7 @@ import { Logo } from "../atoms/Logo";
 
 export const Footer = () => {
 	return (
-		<footer className='mt-10 bottom-0 border-t border-gray-400 py-5 w-full text-center text-sm flex items-center justify-center'>
+		<footer className='mt-96 bottom-0 border-t border-gray-400 py-5 w-full text-center text-sm flex items-end justify-center'>
 			<p className='mr-24'>© Tolga Kara</p>
 		</footer>
 	);
