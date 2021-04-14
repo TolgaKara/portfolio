09a393e34b95f37738a033bc5608eb9491180fc6
Tolga Kara <tolgakara2.0@gmail.com>
2021-04-14 21:46:17 +0200

    Added MainPageLayout Component

---
+++ src/components/layout/MainPageLayout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
+
+export const MainPageLayout = (props) => {
+	return (
+		<motion.div
+			variants={pageAnimation}
+			initial='hidden'
+			animate='show'
+			exit='exit'
+			className='container mx-auto flex justify-between items-center mt-10'
+		>
+			{props.children}
+		</motion.div>
+	);
+};
