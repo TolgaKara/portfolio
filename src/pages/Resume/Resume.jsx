45ff1334cea5cdb49eafc221a92d7c3947cf2a6d
Tolga Kara <tolgakara2.0@gmail.com>
2021-04-19 16:39:52 +0200

    Added ResumeLayout to represent paper

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { MainPageLayout } from "../../components/layout/MainPageLayout";
 import { MainHeading } from "../../components/atoms/MainHeading";
 import { SubNav } from "./components/SubNav";
+import { Education } from "./components/Education";
+import { ResumeLayout } from "./components/ResumeLayout";
 
 export default function Resume() {
 	return (
 		<MainPageLayout col={true}>
-			<MainHeading text='Resume' />
-			<SubNav />
+			<ResumeLayout>
+				<MainHeading text='Resume' />
+				<SubNav />
+				<Education />
+			</ResumeLayout>
 		</MainPageLayout>
 	);
 }
