b00bc2ea820dc5b1ed152aa37d30089e49ebbe4c
Tolga Kara <tolgakara2.0@gmail.com>
2021-04-19 16:35:44 +0200

    Fixed alignment Problem of subnav

--- src/pages/Resume/components/SubNav.jsx
+++ src/pages/Resume/components/SubNav.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const SubNav = () => {
 	return (
-		<ul className='flex space-x-10 font-semibold  mt-3 uppercase'>
+		<ul className='flex justify-start space-x-5 font-semibold  mt-3 uppercase mr-8'>
 			<li className='text-yellow-600'>
 				<a href=''>Education</a>
 			</li>
