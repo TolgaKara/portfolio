119dd47047b6de7b78d49aced8a85889e158f8fb
Tolga Kara <tolgakara2.0@gmail.com>
2021-04-19 13:49:46 +0200

    Added Subnav to the resume

--- src/pages/Resume/components/SubNav.jsx
+++ src/pages/Resume/components/SubNav.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 export const SubNav = () => {
 	return (
-		<ul className='flex space-x-10 font-semibold  mt-3'>
+		<ul className='flex space-x-10 font-semibold  mt-3 uppercase'>
 			<li className='text-yellow-600'>
 				<a href=''>Education</a>
 			</li>
 			<li className='text-yellow-600'>
-				<a href=''>Education</a>
+				<a href=''>Work Experience</a>
 			</li>
 			<li className='text-yellow-600'>
-				<a href=''>Education</a>
+				<a href=''>Skills</a>
 			</li>
 		</ul>
 	);
