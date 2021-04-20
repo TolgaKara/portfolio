7d63a1157d0cb2ee6c275c2d9a957bada7fe2be9
Tolga Kara <tolgakara2.0@gmail.com>
2021-04-20 10:26:08 +0200

    Integrated the component EducationDetails

--- src/pages/Resume/components/Education.jsx
+++ src/pages/Resume/components/Education.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Heading } from "./atom/Heading";
+import { EducationDetails } from "./molecules/EducationDetails";
 
 export const Education = () => {
 	return (
 		<div>
 			<Heading text='Education' />
+			<EducationDetails position='' date='' company='' bullet={[]} techStack={[]} />
 		</div>
 	);
 };
