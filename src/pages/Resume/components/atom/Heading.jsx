55084bf503b11ff7a09597e9e24e7c7129596ff2
Tolga Kara <tolgakara2.0@gmail.com>
2021-04-24 14:34:13 +0200

    Updated the styles of the Heading so it is remarkable differnent from the other elements

--- src/pages/Resume/components/atom/Heading.jsx
+++ src/pages/Resume/components/atom/Heading.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const Heading = ({ text }) => {
-	return <h1 className='text-2xl border-l-4 pl-2 border-yellow-500 font-semibold'>{text}</h1>;
+	return <h1 className='text-2xl border-l-4 pl-2 border-yellow-500 font-semibold mb-5'>{text}</h1>;
 };
