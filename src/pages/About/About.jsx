48ae82ece8a1a21b99b4e5c010b435e6fee35eab
Tolga Kara <tolgakara2.0@gmail.com>
2021-04-24 14:12:38 +0200

    Fixed the Transition Issue in my about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import sanityClient from "../../client.js";
 import tolgaHappy from "../../images/tolga/tolgaHappy.png";
+import { MainPageLayout } from "../../components/layout/MainPageLayout";
 
 export default function About() {
 	const [author, setAuthor] = useState(null);
 
 	return (
-		<div className='container mx-auto flex justify-between items-center' id='about'>
+		<MainPageLayout>
 			<div className='content w-8/12'>
 				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ut sunt recusandae mollitia
@@ -15,6 +16,6 @@ export default function About() {
 				</p>
 			</div>
 			<img className='place-items-center' src={tolgaHappy} alt='' />
-		</div>
+		</MainPageLayout>
 	);
 }
